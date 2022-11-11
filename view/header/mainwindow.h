52c7317e831af500e7f83cd2a65c92bd7fb1b9d8
David Stromberger <Davidstromberger99@gmx.de>
2022-11-11 17:20:33 +0100

    removing test data, removing unused deps

--- view/header/mainwindow.h
+++ view/header/mainwindow.h
@@ -10,8 +10,6 @@
 #include "qtoolbutton.h"
 #include <QMainWindow>
 #include <QMediaPlayer>
-#include <QMediaPlaylist>
-#include <QSortFilterProxyModel>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
